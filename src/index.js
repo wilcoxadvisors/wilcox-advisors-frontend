6700a0dbf6d4a60acd5d111dcf28b8e04fa4b372
wilcoxadvisors <garrettwilcox40@gmail.com>
2025-02-25 17:18:23 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App'; // Your existing homepage component (APP.jsx)
+import LearnMore from './pages/LearnMore'; // New page we’ll create
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/learn-more" element={<LearnMore />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

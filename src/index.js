2bd42dd0bc7064d724754c88f25b0d8fa9d90f58
wilcoxadvisors <garrettwilcox40@gmail.com>
2025-02-27 17:50:25 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App'; // Your existing homepage component (APP.jsx)
-import LearnMore from './pages/LearnMore'; // New page we’ll create
-import './index.css';
+import Home from './pages/Home'; // Home page component
+import LearnMore from './pages/LearnMore'; // Learn More page component
+import AdminDashboard from './components/AdminDashboard'; // Admin dashboard component
+import ClientDashboard from './components/ClientDashboard'; // Client dashboard component
+import { AdminProtectedRoute, ClientProtectedRoute } from './components/ProtectedRoutes'; // Protected route components
+import './index.css'; // Global styles (assumed to exist)
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* Home page route */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Learn More page route */}
         <Route path="/learn-more" element={<LearnMore />} />
+        
+        {/* Protected Admin Dashboard route */}
+        <Route
+          path="/admin-dashboard"
+          element={
+            <AdminProtectedRoute>
+              <AdminDashboard />
+            </AdminProtectedRoute>
+          }
+        />
+        
+        {/* Protected Client Dashboard route */}
+        <Route
+          path="/client-dashboard"
+          element={
+            <ClientProtectedRoute>
+              <ClientDashboard />
+            </ClientProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // Renders the app into the DOM element with id="root" in index.html
 );

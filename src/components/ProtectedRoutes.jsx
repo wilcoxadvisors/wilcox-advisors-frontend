fa2878d9ef0a76a66a39d52d461c7368ff3bfd32
wilcoxadvisors <garrettwilcox40@gmail.com>
2025-03-07 18:09:50 -0600

    Update ProtectedRoutes.jsx

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,14 +1,19 @@
 // src/components/ProtectedRoutes.jsx
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
-function AdminProtectedRoute({ children, isLoggedIn, isAdmin }) {
+function AdminProtectedRoute({ children }) {
+  const { isLoggedIn, isAdmin } = useAuth();
+  
   if (!isLoggedIn || !isAdmin) {
     return <Navigate to="/" />;
   }
   return children;
 }
 
-function ClientProtectedRoute({ children, isLoggedIn, isAdmin }) {
+function ClientProtectedRoute({ children }) {
+  const { isLoggedIn } = useAuth();
+  
   if (!isLoggedIn) {
     return <Navigate to="/" />;
   }
